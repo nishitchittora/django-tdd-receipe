f21d56772efd7a81e2148e575c6f410e245b4ddd
Nishit Chittora <nishitchittora@Nishits-MacBook-Pro-2.local>
2023-08-17 17:48:21 +0530

    Admin and Swagger Documentation added

--- app/core/admin.py
+++ app/core/admin.py
@@ -8,19 +8,36 @@ from core import models
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
-    fieldsets = ((
-        None, {"fields": ("email", "password")}
-    ),
+    fieldsets = (
+        (
+            None, {"fields": ("email", "password")}
+        ),
         (_("Permissions"), {
-            "fields": {
+            "fields": (
                 "is_active", "is_staff", "is_superuser"
-            }
+            )
         }),
         (
-        _("Important Dates"), {"fields": ("last_login",)}
-    )
+            _("Important Dates"), {"fields": ("last_login",)}
+        )
     )
     readonly_fields = ["last_login"]
+    add_fieldsets = (
+        (
+            None, {
+                "classes": ['wide',],
+                "fields": (
+                    'email',
+                    "password1",
+                    "password2",
+                    "name",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser"
+                )
+            }
+        ),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
